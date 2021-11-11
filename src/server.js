9d784d501911c3a430efce9e931193bb7dd5f23f
George Ramirez <george.ramirez2@gmail.com>
2021-11-11 17:28:13 -0600

    added eslint script and v1 router

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ import cors from 'cors'
 
 export const app = express()
 
+const v1 = express.Router()
+
 app.disable('x-powered-by')
 
 app.use(cors())
@@ -12,10 +14,24 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
+app.use('/ssc/api/v1', v1)
+
+//root Router
 app.get('/', (req, res) => {
-  res.send({message: 'hello, this is a test'})
+    res.send({message: 'hello, this is a test'})
 })
 
+app.post('/', (req, res) => {
+    console.log('body:', req)
+    res.send({message: 'req received'})
+})
+
+//v1
+v1.get('/', (req, res)=>{
+    res.send({'message': '/api/v1'})
+})
+
+// Start app
 export const start = () => {
   app.listen( 5000, () => {
     console.log('Server is on 5000')

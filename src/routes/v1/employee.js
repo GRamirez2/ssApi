cba56a522d1acb341df206f07d95f8862039adc3
George Ramirez <george.ramirez2@gmail.com>
2022-10-11 17:45:59 -0500

    added the swagger docs for the GET employee

--- src/routes/v1/employee.js
+++ src/routes/v1/employee.js
@@ -3,8 +3,134 @@ import express from 'express';
 const employee = express.Router()
 const { findAll_employees_GET, createNew_employee_POST, findOne_employee_GET, update_employee_PUT, delete_employee_DELETE}  = require('../../controlers/employeeOperations')
 import { numberValidation } from '../../controlers/helpers';
+ 
+/**
+ * @openapi
+ *  components:
+ *      schema:
+ *          employee:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                  manager_id:
+ *                      type: integer
+ *                  first_name:
+ *                      type: string
+ *                  last_name:
+ *                      type: string
+ *                  active:
+ *                      type: boolean
+ *                  status:
+ *                      type: string
+ *                  appAdmin:
+ *                      type: boolean
+ *                  email:
+ *                      type: string
+ *                  phone:
+ *                      type: string
+ *                  linkedIn:
+ *                      type: string
+ *                  github:
+ *                      type: string
+ *                  start_date:
+ *                      type: string
+ *                  extended:
+ *                      type: boolean
+ *                  extended_start_date:
+ *                      type: string
+ *                  createdAt:
+ *                      type: string
+ *                  updatedAt:
+ *                      type: string
+ *                  Skill:
+ *                      type: object
+ *                      properties:
+ *                          id:
+ *                              type: integer
+ *                          employee_id:
+ *                              type: string
+ *                          primary_tech:
+ *                              type: string
+ *                          secondary_tech:
+ *                              type: string
+ *                          desired_tech:
+ *                              type: string
+ *                          createdAt:
+ *                              type: string
+ *                          updatedAt:
+ *                              type: string
+ *                  Management:
+ *                      type: object
+ *                      properties:
+ *                          id:
+ *                              type: integer
+ *                          first_name:
+ *                              type: string
+ *                          last_name:
+ *                              type: string
+ *                          seniority_level:
+ *                              type: string
+ *                          department:
+ *                              type: string
+ *                          createdAt:
+ *                              type: string
+ *                          updatedAt:
+ *                              type: string
+ *                  Engagement:
+ *                      type: object
+ *                      properties:
+ *                          id:
+ *                              type: integer
+ *                          client:
+ *                              type: string
+ *                          project:
+ *                              type: string
+ *                          start_date:
+ *                              type: string
+ *                          end_date:
+ *                              type: string
+ *                          extended:
+ *                              type: boolean
+ *                          notes:
+ *                              type: string
+ *                          project_type:
+ *                              type: string
+ *                          size:
+ *                              type: integer
+ *                          project_length:
+ *                              type: string
+ *                          createdAt:
+ *                              type: string
+ *                          updatedAt:
+ *                              type: string
+ *                          
+ *                          
+ *              
+ */
 
 
+/**
+ * @openapi
+ * /employee:
+ *  get:
+ *    summary: Returns an object with the number of employees and a list of employee objects
+ *    description: Returns an object with the number of employees and a list of employee objects
+ *    responses:
+ *      200:
+ *          description: length is the number of employees, data is the array of employee objects
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          length:
+ *                              type: integer
+ *                          data:
+ *                              type: array
+ *                              items:
+ *                                  $ref: '#components/schema/employee'
+ */
 employee.route('/')
 .get(findAll_employees_GET)
 .post(createNew_employee_POST)
--- src/server.js
+++ src/server.js
@@ -11,24 +11,28 @@ export const app = express()
 
 const swaggerJSDoc = require ('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+
+const host = `http://${process.env.IP}:${process.env.PORT}`;
+
 const options = {
   definition:{
-    openapi: '3.0.0',
     info: {
-      title: 'API Skills & Engagements for SingleStone Consulting',
-      version: '1.0.0'
+      title: 'SingleStone Consulting: Employee Information + Management/Team + Skills + Engagements',
+      version: '1.0.0',
+      description: 'REST API for employees, their managers and teams, skills and engagements.'
     },
+    openapi: '3.0.0',
     servers: [
       {
-          url: 'http://localhost:1972/'
+          url: 'http://localhost:1972/ssc/api/v1/'
       }
     ]
   },
-  apis: ['./routes/v1/employee.js', './routes/v1/engagement.js', './routes/v1/management.js', './routes/v1/skills.js']
+  apis: ['./src/server.js', './src/routes/v1/employee.js', '.src/routes/v1/engagement.js', '.src/routes/v1/management.js', '.src/routes/v1/skills.js']
 }
 
 const swaggerSpec = swaggerJSDoc(options);
-app.use('/ssc/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/ssc/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 app.disable('x-powered-by')
@@ -47,11 +51,20 @@ app.use(morgan('dev'))
 
 app.use('/ssc/api/v1', v1)
 
-//root Router
+// TODO delete this
+/**
+ * @openapi
+ * /:
+ *  get:
+ *    description: this is just the same test
+ *    responses:
+ *      200:
+ *        description: hello this is a get test at the root, no /ssc/api/v1 needed
+ */
 app.get('/', (req, res) => {
     res.send({message: 'hello, this is a get test at the root, no /ssc/api/v1 needed'})
 })
-
+// TODO delete this
 app.post('/', (req, res) => {
     console.log('req:', req.body)
     // make sure the raw post body is json in postman, or just use insomnia
